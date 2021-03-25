f6fde195c7a3fc28cfc0c0b62a5545a88cdc6aa3
claudemamo <823038+claudemamo@users.noreply.github.com>
2021-03-25 09:50:07 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# smooks-xml-to-edi
+
+```
+mvn clean compile exec:java -Dexec.mainClass=org.smooks.examples.xml2edi.Main
+```
---
+++ src/main/java/org/smooks/examples/xml2edi/Main.java
@@ -0,0 +1,126 @@
+/*-
+ * ========================LICENSE_START=================================
+ * Smooks Example :: EDI-to-XML
+ * %%
+ * Copyright (C) 2020 Smooks
+ * %%
+ * Licensed under the terms of the Apache License Version 2.0, or
+ * the GNU Lesser General Public License version 3.0 or later.
+ * 
+ * SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-or-later
+ * 
+ * ======================================================================
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * ======================================================================
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * =========================LICENSE_END==================================
+ */
+package org.smooks.examples.xml2edi;
+
+import org.smooks.Smooks;
+import org.smooks.SmooksException;
+import org.smooks.container.ExecutionContext;
+import org.smooks.event.report.HtmlReportGenerator;
+import org.smooks.io.StreamUtils;
+import org.smooks.payload.StringResult;
+import org.xml.sax.SAXException;
+
+import javax.xml.transform.stream.StreamSource;
+import java.io.*;
+import java.util.Locale;
+
+/**
+ * Simple example main class.
+ * @author <a href="mailto:tom.fennelly@gmail.com">tom.fennelly@gmail.com</a>
+ */
+public class Main {
+
+    private static byte[] messageIn = readInputMessage();
+
+    protected static String runSmooksTransform() throws IOException, SAXException, SmooksException {
+    	
+    	Locale defaultLocale = Locale.getDefault();
+    	Locale.setDefault(new Locale("en", "IE"));
+    	
+        // Instantiate Smooks with the config...
+        Smooks smooks = new Smooks("smooks-config.xml");
+        try {
+             // Create an exec context - no profiles....
+            ExecutionContext executionContext = smooks.createExecutionContext();
+
+            StringResult result = new StringResult();
+
+            // Configure the execution context to generate a report...
+            executionContext.setEventListener(new HtmlReportGenerator("target/report/report.html"));
+
+            // Filter the input message to the outputWriter, using the execution context...
+            smooks.filterSource(executionContext, new StreamSource(new ByteArrayInputStream(messageIn)), result);
+
+            Locale.setDefault(defaultLocale);
+
+            return result.getResult();
+        } finally {
+            smooks.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, SAXException, SmooksException {
+        System.out.println("\n\n==============Message In==============");
+        System.out.println(new String(messageIn));
+        System.out.println("======================================\n");
+
+        pause("The XML input stream can be seen above.  Press 'enter' to see this stream transformed into EDI...");
+
+        String messageOut = Main.runSmooksTransform();
+
+        System.out.println("==============Message Out=============");
+        System.out.println(messageOut);
+        System.out.println("======================================\n\n");
+
+        pause("And that's it!  Press 'enter' to finish...");
+    }
+
+    private static byte[] readInputMessage() {
+        try {
+            return StreamUtils.readStream(new FileInputStream("input-message.xml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "<no-message/>".getBytes();
+        }
+    }
+
+    private static void pause(String message) {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("> " + message);
+            in.readLine();
+        } catch (IOException e) {
+        }
+        System.out.println("\n");
+    }
+}
